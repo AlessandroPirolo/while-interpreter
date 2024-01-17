9b0fb6c181340bca5b24a292b4ccb25ec776d0e2
AlessandroPirolo <Alessandro.pirolo@studenti.unipd.it>
2024-01-17 10:48:24 +0100

    Create Program.fs
    Main file of the interpreter

---
+++ Program.fs
@@ -0,0 +1,20 @@
+﻿module Program
+
+open Interp
+open System
+
+
+let print_colored color text = Console.Write(sprintf "\x1b[%sm%s\x1b[0m" color text)
+
+[<EntryPoint>]
+let main argv =
+    print_colored "32;0" "Loading source file...\n"
+    let stmt = System.IO.File.ReadAllText argv.[0]
+    let r = 
+        try
+            printfn "Start computing..."
+            start stmt
+        with e -> printfn "\n exception caught: %O" e; Map ["err", 0]
+    printfn "%A" r
+    0
+
