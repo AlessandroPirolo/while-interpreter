4d0464bc86e3f3d4ce09347189a7fe105a91da1d
AlessandroPirolo <Alessandro.pirolo@studenti.unipd.it>
2024-03-06 12:55:43 +0100

    Create Interp.hs

---
+++ Haskell/while-interpreter/src/Interp/Interp.hs
@@ -0,0 +1,35 @@
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+{-# HLINT ignore "Use tuple-section" #-}
+module Interp.Interp ( start ) where
+
+import Parser.Ast ( Statement(..), State )
+import Data.Map as Map ( fromListWith )
+import Data.List as List ( map, singleton )
+import Eval.Evaluator ( eval )
+import Parser.Parser ( whileParser )
+import qualified Data.Text as T
+import Text.ParserCombinators.Parsec (parse)
+
+start :: String -> State
+start stmt = let
+    result = case parse whileParser "" stmt of
+        Right program -> program
+        Left e -> error $ show e
+    vars = findStateVar result
+    state = createState vars
+    in eval result state
+
+findStateVar :: Statement -> [String]
+findStateVar (Assignment var _)                 = [var]
+findStateVar (PairAssignment (var1, var2) _)    = [var1, var2]
+findStateVar (OpAssignment _ var _)             = [var]
+findStateVar Skip                               = []
+findStateVar (Conditional _ t f)                = findStateVar t ++ findStateVar f
+findStateVar (Composition (stmt:stmts))         = findStateVar stmt ++ findStateVar (Composition stmts)
+findStateVar (Composition [])                   = []
+findStateVar (While _ stmt)                     = findStateVar stmt
+findStateVar (Repeat stmt _)                    = findStateVar stmt
+findStateVar (For i _ _ stmt)                   = i : findStateVar stmt
+
+createState :: [String] -> State
+createState = Map.fromListWith (+) . List.map (\key -> (key, 0))
